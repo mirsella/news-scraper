4600472234ece3715e6aa31d2468905cfc376f04
mirsella <mirsella@protonmail.com>
2023-08-20 10:43:47 +0200

    gamp

--- src/newsfetcher/francetvinfo.rs
+++ src/newsfetcher/francetvinfo.rs
@@ -1,7 +1,12 @@
 use super::News;
+use anyhow::Result;
 use headless_chrome::Tab;
 use std::sync::Arc;
 
-pub fn get_news(_tab: Arc<Tab>) -> anyhow::Result<Vec<crate::newsfetcher::News>> {
-    Ok(vec![News::default()])
+pub fn get_news(_tab: Arc<Tab>) -> Result<Vec<News>> {
+    let n = News {
+        time: chrono::Utc::now(),
+        ..News::default()
+    };
+    Ok(vec![n])
 }
