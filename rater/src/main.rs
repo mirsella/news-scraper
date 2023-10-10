d458c6498d5c61dcae4a265ad5531c023498f767
mirsella <mirsella@protonmail.com>
2023-10-10 22:50:39 +0200

    gamp

--- rater/src/main.rs
+++ rater/src/main.rs
@@ -5,11 +5,24 @@ use shared::{config::Config, db_news::DbNews};
 use std::process::exit;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
+use surrealdb::engine::remote::ws::Ws;
 use surrealdb::{
-    engine::remote::ws::Ws,
+    engine::remote::ws::Client as WsClient,
     opt::{auth::Root, RecordId},
     Surreal,
 };
+use tokio::sync::Semaphore;
+
+async fn retrieve_db_news(db: Arc<Surreal<WsClient>>) -> Result<Vec<DbNews>> {
+    let db_news: Option<Vec<DbNews>> = db
+        .query("select * from news where rating == none AND date > time::floor(time::now(), 1w)")
+        .await?
+        .take(0)?;
+    match db_news {
+        Some(db_news) => Ok(db_news),
+        None => Err(anyhow!("no news found")),
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -35,21 +48,28 @@ async fn main() -> Result<()> {
     })
     .await?;
     db.use_ns("news").use_db("news").await?;
+    let db = Arc::new(db);
 
     let openai =
         ChatClient::with_config(OpenAIConfig::default().with_api_key(&config.openai_api_key));
+    let openai = Arc::new(openai);
 
-    // TODO: use semaphore to limit parallel rating
-    // let sem = Arc::new(tokio::sync::Semaphore::new(10))
+    let sem = Arc::new(Semaphore::new(config.parallel_rating));
 
-    let news: Option<Vec<DbNews>> = db
-        .query("select * from news where rating == none AND date > time::floor(time::now(), 1w)")
-        .await?
-        .take(0)?;
-
-    let res: Result<()> = loop {
+    loop {
+        let db_news = retrieve_db_news(db.clone()).await;
+        let db_news = match db_news {
+            Ok(news) => news,
+            Err(e) if e.to_string() == "no news found" => {
+                trace!("no news to process");
+                tokio::time::sleep(tokio::time::Duration::from_secs(60)).await;
+                continue;
+            }
+        }
+    }
+    for news in db_news {
         if !running.load(Ordering::Relaxed) {
-            break Ok(());
+            return Ok(());
         }
         let mut news = match news {
             Err(e) if e.to_string() == "no news found" => {
@@ -57,7 +77,7 @@ async fn main() -> Result<()> {
                 tokio::time::sleep(tokio::time::Duration::from_secs(60)).await;
                 continue;
             }
-            Err(e) => break Err(e.context("failed to get news from db")),
+            Err(e) => return Err(e.context("failed to get news from db")),
             Ok(news) => news,
         };
         let id = news.id.clone().expect("no id wtf");
@@ -74,6 +94,6 @@ async fn main() -> Result<()> {
         if let Err(e) = news.save(&db).await {
             error!("save: {e}")
         }
-    };
-    res
+    }
+    Ok(())
 }
