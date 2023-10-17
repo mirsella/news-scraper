9ac739ee2dead0add784ce1920de2f18a0314661
mirsella <mirsella@protonmail.com>
2023-10-17 14:00:33 +0200

    gamp

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,21 +1,18 @@
 <script setup lang="ts">
 const { $db } = useNuxtApp();
-// setInterval(async () => {
-//   // console.log(await $db.ready, $db.status, await $db.info());
-//   let query = $db.select("news");
-//   console.log(await query);
-// }, 500);
-
-// setTimeout(async () => {
-//   await $db.connect("http://127.0.0.1:8000/rpc", {
-//     ns: "news",
-//     db: "news",
-//     auth: {
-//       user: "news",
-//       pass: "arst",
-//     },
-//   });
-// }, 2000);
+(async () => {
+  console.log("connected", $db.ready);
+})();
+setTimeout(async () => {
+  await $db.connect("http://127.0.0.1:8000/rpc", {
+    ns: "news",
+    db: "news",
+    auth: {
+      user: "news",
+      pass: "arst",
+    },
+  });
+}, 2000);
 </script>
 
 <template>
--- nuxt/plugins/surrealdb.client.ts
+++ nuxt/plugins/surrealdb.client.ts
@@ -1,15 +1,48 @@
 import { Surreal } from "surrealdb.js";
 
+/*
+if no token is found, we need to show a login component
+*/
+
+const db = new Surreal();
+const authenticated = ref(false);
+
+async function connect() {
+  const surrealdb_url = process.env.surrealdb_url;
+  if (!surrealdb_url) {
+    throw new Error("surrealdb_url is not set");
+  }
+  return db.connect(surrealdb_url, {
+    ns: "news",
+    db: "news",
+  });
+}
+
+async function login() {
+  const jwt = localStorage.getItem("jwt");
+  if (!jwt) {
+    // TODO: show login page
+    return;
+  }
+  const authenticated = await db.authenticate(jwt);
+  if (authenticated) {
+    // TODO: show succesfull login
+  } else {
+    // TODO: show login page "connection expired"
+  }
+}
+
 export default defineNuxtPlugin({
   name: "surrealdb",
-  parallel: false,
-  setup(NuxtApp) {
-    const db = new Surreal();
-    const authenticated = ref(false);
-
+  parallel: true,
+  async setup(NuxtApp) {
     return {
       provide: {
         db: db,
+        dbhelper: {
+          authenticated,
+          login,
+        },
       },
     };
   },
