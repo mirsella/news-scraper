1c2360a2d63be817cfefcbf3bfa5a2af29d70719
mirsella <mirsella@protonmail.com>
2023-11-03 11:57:58 +0100

    gamp

--- fetcher/src/sources/mod.rs
+++ fetcher/src/sources/mod.rs
@@ -49,10 +49,7 @@ pub fn fetch_article(url: &str) -> Result<ApiResponse, anyhow::Error> {
             return Err(anyhow!("{}", e));
         }
     };
-    let text = response.into_string()?;
-    info!("response text: {:#?}", &text);
-    // let json_result: ApiResponse = response.into_json()?;
-    let json_result: ApiResponse = serde_json::from_str(&text)?;
+    let json_result: ApiResponse = response.into_json()?;
     Ok(json_result)
 }
 pub fn parse_article(str: &str) -> Result<ApiResponse, anyhow::Error> {
