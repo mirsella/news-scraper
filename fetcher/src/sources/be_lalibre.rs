7794fa9832d0aeb6943e73e5a2b673e28f2574a8
mirsella <mirsella@protonmail.com>
2024-08-20 23:49:11 +0200

    fix be::lalibre

--- fetcher/src/sources/be_lalibre.rs
+++ fetcher/src/sources/be_lalibre.rs
@@ -2,11 +2,11 @@ use super::{GetNewsOpts, News};
 use anyhow::{bail, Context, Result};
 use headless_chrome::Tab;
 use log::{error, info};
-use std::sync::Arc;
+use std::{sync::Arc, thread, time::Duration};
 
 fn get_articles_links(tab: &Arc<Tab>) -> Result<Vec<String>> {
     let links: Vec<String> = tab
-        .find_elements(".ap-StoryList-itemLink:not([href^='/dossier']):not([href^='/videos'])'")
+        .find_elements(".ap-StoryList-itemLink:not([href^='/dossier']):not([href^='/videos'])")
         .context("find_elements articles links")?
         .iter()
         .map(|el| {
@@ -22,14 +22,14 @@ pub fn get_news(opts: GetNewsOpts) -> Result<()> {
     let user_agent = opts.browser.get_version()?.user_agent;
     let user_agent = user_agent.replace("HeadlessChrome", "Chrome");
     tab.set_user_agent(&user_agent, None, None)?;
-    tab.navigate_to("https://www.rtl.be/")
+    tab.navigate_to("https://www.lalibre.be/belgique")
         .context("navigate_to")?;
     tab.wait_until_navigated().context("wait_until_navigated")?;
 
-    // if let Ok(cookie) = tab.find_element("#didomi-notice-agree-button") {
-    //     cookie.click().context("clicking on cookie")?;
-    //     thread::sleep(Duration::from_secs(1));
-    // }
+    if let Ok(cookie) = tab.wait_for_element("#didomi-notice-agree-button") {
+        cookie.click().context("clicking on cookie")?;
+        thread::sleep(Duration::from_secs(1));
+    }
 
     let links = get_articles_links(&tab)?;
     info!("found {} articles", links.len());
