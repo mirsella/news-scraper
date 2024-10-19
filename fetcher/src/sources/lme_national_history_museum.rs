ef199e3f5af949250bd31ce966c7a9e360f093be
mirsella <mirsella@protonmail.com>
2024-10-19 14:13:31 +0200

    lme::nhm remove default tags

--- fetcher/src/sources/lme_national_history_museum.rs
+++ fetcher/src/sources/lme_national_history_museum.rs
@@ -38,30 +38,18 @@ pub fn get_news(opts: GetNewsOpts) -> Result<()> {
         if opts.is_seen(&url) {
             continue;
         }
-        let tags: Vec<_> = [
-            "national history museum",
-            "wildlife",
-            "photography",
-            "nhm",
-            "wpo",
-            "lemediaexperience",
-        ]
-        .into_iter()
-        .map(str::to_string)
-        .collect();
-
         let payload = match fetch_article(&url) {
             Ok(res) => Ok(News {
                 title: res.title,
                 caption: res.description,
                 provider: opts.provider.clone(),
-                tags,
                 date: res
                     .published
                     .parse()
                     .unwrap_or_else(|_| chrono::Local::now()),
                 body: res.content,
                 link: url,
+                ..Default::default()
             }),
             Err(err) => {
                 debug!("fetch_article on {url}: {err}");
