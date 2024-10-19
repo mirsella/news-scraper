0c5e21fcd3fe3801077c9e451bf2758643a72a99
mirsella <mirsella@protonmail.com>
2024-10-19 14:25:21 +0200

    fix bbcearth

--- fetcher/src/sources/lme_bbcearth.rs
+++ fetcher/src/sources/lme_bbcearth.rs
@@ -15,10 +15,11 @@ fn get_articles_links(tab: &Arc<Tab>) -> Result<Vec<String>> {
         .context("finding articles links")?
         .iter()
         .filter_map(|a| {
-            let link = a.get_attribute_value("href").unwrap().expect("a href");
+            let mut link = a.get_attribute_value("href").unwrap().expect("a href");
             if link.contains("/quiz/") {
                 return None;
             }
+            link.insert_str(0, "https://www.bbcearth.com");
             Some(link)
         })
         .collect())
