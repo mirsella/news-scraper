69d3a9f6ff2dc1c9327fd63c6b828ffaeff8669f
mirsella <mirsella@protonmail.com>
2024-10-19 14:25:31 +0200

    update smithsonianmag

--- fetcher/src/sources/lme_smithsonianmag.rs
+++ fetcher/src/sources/lme_smithsonianmag.rs
@@ -9,8 +9,8 @@ use std::sync::Arc;
 
 fn get_articles_links(tab: &Arc<Tab>) -> Result<Vec<String>> {
     Ok(tab
-        .find_elements(".article-list-text")
-        .context("find_elements .article-list-text")?
+        .find_elements(".article-list-text > h3 > a")
+        .context("finding articles links")?
         .iter()
         .map(|el| {
             let mut link = el
@@ -32,7 +32,7 @@ pub fn get_news(opts: GetNewsOpts) -> Result<()> {
     let user_agent = opts.browser.get_version().unwrap().user_agent;
     let user_agent = user_agent.replace("HeadlessChrome", "Chrome");
     tab.set_user_agent(&user_agent, None, None)?;
-    tab.navigate_to("https://www.smithsonianmag.com/category/travel/")
+    tab.navigate_to("https://www.smithsonianmag.com")
         .context("navigate_to")?
         .wait_until_navigated()
         .context("wait_until_navigated")?;
