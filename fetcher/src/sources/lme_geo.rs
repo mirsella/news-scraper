b42717324b3b27e93d39dc8bbb73ef4a1d78e978
mirsella <mirsella@protonmail.com>
2024-10-22 17:08:11 +0200

    improve lme::geo with more category

--- fetcher/src/sources/lme_geo.rs
+++ fetcher/src/sources/lme_geo.rs
@@ -10,9 +10,15 @@ use std::sync::Arc;
 fn get_articles_links(tab: &Arc<Tab>) -> Result<Vec<String>> {
     Ok(tab
         .find_elements("h2.articlePreview-title > a")
-        .context("finding h2.articlePreview-title > a")?
+        .context("finding articles links")?
         .iter()
-        .map(|a| a.get_attribute_value("href").unwrap().expect("a href"))
+        .filter_map(|a| {
+            let link = a.get_attribute_value("href").unwrap().expect("a href");
+            if link.ends_with("geo.fr/") {
+                return None;
+            }
+            Some(link)
+        })
         .collect())
 }
 
@@ -22,41 +28,42 @@ pub fn get_news(opts: GetNewsOpts) -> Result<()> {
     let user_agent = opts.browser.get_version().unwrap().user_agent;
     let user_agent = user_agent.replace("HeadlessChrome", "Chrome");
     tab.set_user_agent(&user_agent, None, None)?;
-    tab.navigate_to("https://www.geo.fr/aventure")
-        .context("navigate_to")?
-        .wait_until_navigated()
-        .context("wait_until_navigated")?;
-    let links = get_articles_links(&tab).context("get_articles_links")?;
-    info!("found {} articles", links.len());
-    if links.is_empty() {
-        bail!("no links found");
-    }
-    for url in links {
-        if opts.is_seen(&url) {
-            continue;
-        }
-
-        tab.navigate_to(&url)
-            .context(format!("navigate_to {url}"))?
+    for category in ["evenement/vacances-en-france", "voyage", "aventure"] {
+        tab.navigate_to(&format!("https://www.geo.fr/{category}"))
+            .context("navigate_to")?
             .wait_until_navigated()
-            .context("wait_until_navigated url")?;
-        let body = tab.get_content()?;
-        let payload = match parse_article(&body) {
-            Ok(res) => Ok(News {
-                title: res.title,
-                caption: res.description,
-                provider: opts.provider.clone(),
-                date: res.published,
-                body: res.content,
-                link: url,
-                ..Default::default()
-            }),
-            Err(err) => {
-                log::warn!("parse_article on {url}: {err}");
+            .context("wait_until_navigated")?;
+        let links = get_articles_links(&tab).context("get_articles_links")?;
+        info!("found {} articles", links.len());
+        if links.is_empty() {
+            bail!("no links found");
+        }
+        for url in links {
+            if opts.is_seen(&url) {
                 continue;
             }
-        };
-        opts.tx.blocking_send(payload)?;
+            tab.navigate_to(&url)
+                .context(format!("navigate_to {url}"))?
+                .wait_until_navigated()
+                .context("wait_until_navigated url")?;
+            let body = tab.get_content()?;
+            let payload = match parse_article(&body) {
+                Ok(res) => Ok(News {
+                    title: res.title,
+                    caption: res.description,
+                    provider: opts.provider.clone(),
+                    date: res.published,
+                    body: res.content,
+                    link: url,
+                    ..Default::default()
+                }),
+                Err(err) => {
+                    log::warn!("parse_article on {url}: {err:?}");
+                    continue;
+                }
+            };
+            opts.tx.blocking_send(payload)?;
+        }
     }
     Ok(())
 }
