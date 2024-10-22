ff468da5d6cf0a8acbb6eae588f09d7f968739ed
mirsella <mirsella@protonmail.com>
2024-10-22 16:44:39 +0200

    fix lme::futurasciences

--- fetcher/src/sources/lme_futura_sciences.rs
+++ fetcher/src/sources/lme_futura_sciences.rs
@@ -1,5 +1,5 @@
 use super::GetNewsOpts;
-use crate::sources::parse_article;
+use crate::sources::fetch_article;
 use anyhow::bail;
 use anyhow::{Context, Result};
 use headless_chrome::Tab;
@@ -16,7 +16,7 @@ fn get_articles_links(tab: &Arc<Tab>) -> Result<Vec<String>> {
         .iter()
         .filter_map(|a| {
             if let Some(mut link) = a.get_attribute_value("href").unwrap() {
-                if link.contains("futura-sciences.com/live") {
+                if link.contains("/personnalites") || link.contains("/live") {
                     return None;
                 }
                 link.insert_str(0, "https://futura-sciences.com");
@@ -49,52 +49,15 @@ pub fn get_news(opts: GetNewsOpts) -> Result<()> {
             if opts.is_seen(&url) {
                 continue;
             }
-
-            let tags: Vec<_> = url
-                .strip_prefix("https://futura-sciences.com/")
-                .expect(&url)
-                .split('/')
-                .take(2)
-                .chain(["lemediaexperience"].into_iter())
-                .map(str::to_string)
-                .collect();
-
-            tab.navigate_to(&url)
-                .context(format!("navigate_to {url}"))?
-                .wait_until_navigated()
-                .context("wait_until_navigated url")?;
-            let body = tab.get_content()?;
-
-            tab.evaluate(
-            "document.querySelectorAll('.article-sidebar, .relative, .bottom2').forEach(e => e.remove())",
-            false,
-        )?;
-
-            let mut imgs_els = tab
-                .find_element(".article-content")
-                .context("find_element .article-content")?
-                .find_elements("img")
-                .context("find_elements .article-content img")?;
-            imgs_els.push(
-                tab.find_element(".article-hero-image")
-                    .context("find_elements .article-hero-image")?
-                    .find_element("img")
-                    .context("find_element article-hero-image img")?,
-            );
-            let imgs = imgs_els.iter().fold(String::new(), |mut s, el| {
-                let src = el.get_attribute_value("src").unwrap().expect("a src");
-                s += &format!("<img src='{src}' />");
-                s
-            });
-            let payload = match parse_article(&body) {
+            let payload = match fetch_article(&url) {
                 Ok(res) => Ok(News {
                     title: res.title,
                     caption: res.description,
                     provider: opts.provider.clone(),
-                    tags,
-date: res.published,
-                    body: imgs + &res.content,
+                    date: res.published,
+                    body: res.content,
                     link: url,
+                    ..Default::default()
                 }),
                 Err(err) => {
                     log::warn!("parse_article on {url}: {err}");
