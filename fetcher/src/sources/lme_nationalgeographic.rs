31d11ece857d4ea477372c9da168817e3c31d611
mirsella <mirsella@protonmail.com>
2024-10-22 16:44:43 +0200

    fix nationalgeographic

--- fetcher/src/sources/lme_nationalgeographic.rs
+++ fetcher/src/sources/lme_nationalgeographic.rs
@@ -1,27 +1,28 @@
 use super::GetNewsOpts;
-use crate::sources::parse_article;
+use crate::sources::fetch_article;
 use anyhow::bail;
 use anyhow::{Context, Result};
 use headless_chrome::Tab;
 use log::{info, trace};
 use shared::News;
-use std::{sync::Arc, thread, time::Duration};
+use std::collections::HashSet;
+use std::sync::Arc;
 
 fn get_articles_links(tab: &Arc<Tab>) -> Result<Vec<String>> {
     Ok(tab
-        .find_element("#hub-page-first-page-content")
-        .context("finding .hub-page-first-page-content")?
-        .find_elements("a[title][href]")
-        .context("finding a[title][href]")?
+        .find_elements("#hub-page-first-page-content a[title][href]:not([target])")
+        .context("finding articles links")?
         .iter()
         .filter_map(|a| {
             let mut link = a.get_attribute_value("href").unwrap().expect("a href");
-            link.insert_str(0, "https://www.nationalgeographic.fr");
             if link.contains("contenu-sponsorise") {
                 return None;
             }
+            link.insert_str(0, "https://www.nationalgeographic.fr");
             Some(link)
         })
+        .collect::<HashSet<_>>()
+        .into_iter()
         .collect())
 }
 
@@ -31,7 +32,7 @@ pub fn get_news(opts: GetNewsOpts) -> Result<()> {
     let user_agent = opts.browser.get_version().unwrap().user_agent;
     let user_agent = user_agent.replace("HeadlessChrome", "Chrome");
     tab.set_user_agent(&user_agent, None, None)?;
-    for category in ["voyage", "environnement"] {
+    for category in ["voyage", "environnement", "histoire"] {
         trace!("checking out category {}", category);
         tab.navigate_to(&format!("https://www.nationalgeographic.fr/{category}"))
             .context("navigate_to")?
@@ -46,35 +47,13 @@ pub fn get_news(opts: GetNewsOpts) -> Result<()> {
             if opts.is_seen(&url) {
                 continue;
             }
-            tab.navigate_to(&url)
-                .context(format!("navigate_to {url}"))?
-                .wait_until_navigated()
-                .context("wait_until_navigated url")?;
-
-            tab.evaluate("setInterval(() => window.scrollBy(0, 1000), 50)", false)?;
-            thread::sleep(Duration::from_secs(1));
-            tab.evaluate(
-                "document.querySelectorAll('.css-1082emh').forEach(e => e.remove())",
-                false,
-            )?;
-            let imgs_els = &tab
-                .find_element("article")
-                .context("find_element article")?
-                .find_elements("img")
-                .context("find_elements img")?;
-            let imgs = imgs_els.iter().fold(String::new(), |mut s, el| {
-                let src = el.get_attribute_value("src").unwrap().expect("a src");
-                s += &format!("<img src='{src}' />");
-                s
-            });
-            let body = tab.get_content()?;
-            let payload = match parse_article(&body) {
+            let payload = match fetch_article(&url) {
                 Ok(res) => Ok(News {
                     title: res.title,
                     caption: res.description,
                     provider: opts.provider.clone(),
                     date: res.published,
-                    body: imgs.clone() + &res.content,
+                    body: res.content,
                     link: url,
                     ..Default::default()
                 }),
